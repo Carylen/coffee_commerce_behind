b18fd16af51ba6f98c8f9a4250f8fccedeaf7d1b
irhamZharfan <irham.atmoko@ids.id>
2025-05-14 11:07:38 +0700

    adding vercel.json

--- index.js
+++ index.js
@@ -6,26 +6,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const table_name = process.env.TABLE_TOKEN
+
 // Simple route
 app.get('/', async (req, res) => {
     // const rows = await sql.query('SELECT *  FROM users WHERE user_id = ANY($1)', [[1, 2]])
-    const rows = await sql`SELECT * FROM token_usage_history`;
+    const rows = await sql`SELECT * FROM ${sql.unsafe(table_name)}`;
     // const user_name = rows.map((e) => (e.user_name))
     console.log(rows)
     res.send(rows);
 });
 
 app.post('/insert-token', async(req, res) => {
-    const {token_type, total_token} = req.body
-
-    if (!token_type || !total_token) {
-        return res.status(400).json({ error: 'Missing token_type or total_token' });
-    }
+    const {token_type, token_usage} = req.body
+    let cost = 0
     
+    if (!token_type || !token_usage) {
+        return res.status(400).json({ error: 'Missing token_type or token_usage' });
+    }
+
+    if (token_type.trim().toLowerCase() == 'input_token') {
+        // For calculate the input_token
+        cost = (token_usage / 1000000 * (16520 * 0.075), 2).toFixed(2)
+    }
+    else{
+        // For calculate the output_token
+        cost = (token_usage / 1000000 * (16520 * 0.30), 2).toFixed(2)
+    }
     try {
         const result = await sql`
-        INSERT INTO token_usage_history (token_type, total_token)
-        VALUES (${token_type}, ${total_token})
+        INSERT INTO ${sql.unsafe(table_name)} (token_type, token_usage, total_cost)
+        VALUES (${token_type}, ${token_usage}, ${cost})
         RETURNING *;
         `;
 
@@ -39,5 +50,6 @@ app.post('/insert-token', async(req, res) => {
 // Start server
 const PORT = 3000;
 app.listen(PORT, () => {
+    console.log(`Table Name : ${table_name}`)
   console.log(`Server running on port ${PORT}`);
 });
